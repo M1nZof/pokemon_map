764b825ed7500d17a42262a94a30f36c65ce3b29
M1nZof <maxykl38@gmail.com>
2023-02-26 22:22:03 +0300

    Define required and optional fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200, verbose_name='Имя покемона на русском')
-    title_en = models.CharField(max_length=200, blank=True, verbose_name='Имя покемона на английском')
-    title_jp = models.CharField(max_length=200, blank=True, verbose_name='Имя покемона на японском')
+    title_en = models.CharField(max_length=200, verbose_name='Имя покемона на английском', null=True, blank=True)
+    title_jp = models.CharField(max_length=200, verbose_name='Имя покемона на японском', null=True, blank=True)
     description = models.TextField(blank=True, verbose_name='Описание покемона')
     evolved_from = models.ForeignKey("self", null=True, on_delete=models.CASCADE,
                                      related_name="pokemons", related_query_name="pokemon_evolution",
@@ -17,15 +17,15 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
-    lat = models.FloatField(verbose_name='Широта')
-    lon = models.FloatField(verbose_name='Долгота')
-    appeared_at = models.DateTimeField(verbose_name='Время появления')
-    disappear_at = models.DateTimeField(verbose_name='Время исчезновения')
-    level = models.IntegerField(default=1, verbose_name='Уровень покемона')
-    health = models.IntegerField(default=100, verbose_name='Здоровье покемона')
-    strength = models.IntegerField(default=10, verbose_name='Сила покемона')
-    defence = models.IntegerField(default=5, verbose_name='Защита покемона')
-    stamina = models.IntegerField(default=100, verbose_name='Стамина (выносливость) покемона')
+    lat = models.FloatField(verbose_name='Широта', null=True)
+    lon = models.FloatField(verbose_name='Долгота', null=True)
+    appeared_at = models.DateTimeField(verbose_name='Время появления', null=True)
+    disappear_at = models.DateTimeField(verbose_name='Время исчезновения', null=True)
+    level = models.IntegerField(verbose_name='Уровень покемона', null=True, blank=True)
+    health = models.IntegerField(verbose_name='Здоровье покемона', null=True, blank=True)
+    strength = models.IntegerField(verbose_name='Сила покемона', null=True, blank=True)
+    defence = models.IntegerField(verbose_name='Защита покемона', null=True, blank=True)
+    stamina = models.IntegerField(verbose_name='Стамина (выносливость) покемона', null=True, blank=True)
 
     def __str__(self):
         return f'lat - {self.lat} : lon - {self.lon}'
