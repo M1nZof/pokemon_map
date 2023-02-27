f4cf76bf3d38e60a70f28f966c59eece7df5dd73
M1nZof <maxykl38@gmail.com>
2023-02-28 00:15:30 +0300

    Changed /media path to MEDIA_URL from settings

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.utils.timezone import localtime
 
+from pogomap.settings import MEDIA_URL
 from pokemon_entities.models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -35,7 +36,7 @@ def show_all_pokemons(request):
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons_entities:
-        pokemon_image = request.build_absolute_uri(f'media/{pokemon.pokemon.image}')
+        pokemon_image = request.build_absolute_uri(f'{MEDIA_URL}/{pokemon.pokemon.image}')
         add_pokemon(
             folium_map, pokemon.lat,
             pokemon.lon,
@@ -45,7 +46,7 @@ def show_all_pokemons(request):
     pokemons_on_page = []
     pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
-        pokemon_image = request.build_absolute_uri(f'media/{pokemon.image}')
+        pokemon_image = request.build_absolute_uri(f'{MEDIA_URL}/{pokemon.image}')
         pokemons_on_page.append({
             'pokemon_id': pokemon.pk,
             'img_url': pokemon_image,
@@ -65,7 +66,7 @@ def show_pokemon(request, pokemon_id):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemons_entities:
         if pokemon_entity.appeared_at > localtime() or pokemon_entity.disappear_at > localtime():
-            pokemon_image = request.build_absolute_uri(f'../../media/{pokemon.image}')
+            pokemon_image = request.build_absolute_uri(f'../../{MEDIA_URL}/{pokemon.image}')
             add_pokemon(
                 folium_map, pokemon_entity.lat,
                 pokemon_entity.lon,
@@ -74,7 +75,7 @@ def show_pokemon(request, pokemon_id):
 
     previous_evolution = None
     if pokemon.evolved_from:
-        previous_evolution_pokemon_image = request.build_absolute_uri(f'../../media/{pokemon.evolved_from.image}')
+        previous_evolution_pokemon_image = request.build_absolute_uri(f'../../{MEDIA_URL}/{pokemon.evolved_from.image}')
         previous_evolution = {
             'title_ru': pokemon.evolved_from.title_ru,
             'pokemon_id': pokemon.evolved_from.pk,
@@ -87,14 +88,14 @@ def show_pokemon(request, pokemon_id):
         next_evolution = None
 
     if next_evolution:
-        next_evolution_pokemon_image = request.build_absolute_uri(f'../../media/{next_evolution.image}')
+        next_evolution_pokemon_image = request.build_absolute_uri(f'../../{MEDIA_URL}/{next_evolution.image}')
         next_evolution = {
             "title_ru": next_evolution.title_ru,
             "pokemon_id": next_evolution.pk,
             "img_url": next_evolution_pokemon_image
         }
 
-    pokemon_image = request.build_absolute_uri(f'../../media/{pokemon.image}')
+    pokemon_image = request.build_absolute_uri(f'../../{MEDIA_URL}/{pokemon.image}')
     pokemon_page = {
         'img_url': pokemon_image,
         'title_ru': pokemon.title_ru,
